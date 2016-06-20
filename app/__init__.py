384052c46322ca28807b9030b26a5d20c4a3c9c8
Jon Ander Besga <jonan.bsg@gmail.com>
2016-06-20 22:20:03 +0200

    working fine now

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,16 @@
 from flask import Flask, render_template, request
-from app.cipher.caesar import Caesar
+from .cipher import Caesar
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    c = Caesar(3)
     return render_template('index.html')
 
 @app.route("/encrypt/", methods=['POST'])
 def encrypt():
-    try:
-        c = Caesar(request.json['key'])
-    except ValueError:
-        return 'ValueError in key'
-    try:
-        encrypted_sentence = c.encrypt_sentence(request.json['sentence'])
-    except ValueError:
-        return 'ValueError in sentence'
+    c = Caesar(request.json['key'])
+    encrypted_sentence = c.encrypt_sentence(request.json['sentence'])
     return str(encrypted_sentence)
 
 @app.route("/decrypt/", methods=['POST'])
