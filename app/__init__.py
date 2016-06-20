80c7236f06cf400620ae15b1f5a7a0113fe772cd
Jon Ander Besga <jonan.bsg@gmail.com>
2016-06-20 21:56:21 +0200

    changed structure of the project to match heroku requirements (gunicorn app)

---
+++ app/__init__.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+from .cipher import Caesar
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/encrypt", methods=['POST'])
+def encrypt():
+    c = Caesar(request.json['key'])
+    encrypted_sentence = c.encrypt_sentence(request.json['sentence'])
+    return str(encrypted_sentence)
+
+@app.route("/decrypt", methods=['POST'])
+def decrypt():
+    c = Caesar(request.json['key'])
+    decrypted_sentence = c.decrypt_sentence(request.json['sentence'])
+    return str(decrypted_sentence)
